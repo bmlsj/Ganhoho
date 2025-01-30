1cea8ce2a11810d238c9915a328a06852c7c13ca
I'mplant <152685022+KR-ImPlant@users.noreply.github.com>
2025-01-31 06:15:22 +0900

    S12P11D209-145 fix: 친구추가 API

--- src/main/java/com/ssafy/ganhoho/domain/friend/dto/FriendAddResponse.java
+++ src/main/java/com/ssafy/ganhoho/domain/friend/dto/FriendAddResponse.java
@@ -5,7 +5,6 @@ import lombok.*;
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class FriendAddResponse {
     // 딱히 수락은 DTO 줄필요가...
 }
