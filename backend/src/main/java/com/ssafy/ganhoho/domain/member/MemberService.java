96aa130d4b7bf0a8c6ec8a5a37c622225007b0f8
hay6586 <hay6586@gmail.com>
2025-02-03 14:28:53 +0900

    Revert "Merge branch 'develop-back' into 'develop'"
    This reverts commit 8280197262a6a156d03a29923de81f63b127c281, reversing
    changes made to c79072af39fb89bcbdc01db4cb895021dce271d9.

--- backend/src/main/java/com/ssafy/ganhoho/domain/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ganhoho.domain.member;
-
-import com.ssafy.ganhoho.domain.member.dto.MemberInfoResponse;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface MemberService {
-    MemberInfoResponse getMemberInfo(Long memberId);
-}
--- backend/src/main/java/com/ssafy/ganhoho/domain/schedule/entity/WorkType.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.ganhoho.domain.schedule.entity;
-
-public enum WorkType {
-    D, N, E, OF
-} 
