14da8e6b6d37ed32601e2f0490ad1164c7508d3d
황홍법 <bup553@naver.com>
2025-02-17 21:30:38 +0900

    test:전체근무페이지view css수정
    height을 80vh로 content의 height을 100%로

--- common-project/src/views/FullWorkScheduleView.vue
+++ common-project/src/views/FullWorkScheduleView.vue
@@ -130,7 +130,7 @@ onUnmounted(() => {
   margin: 0 auto;
   padding: 16px;
   flex-direction: column;
-  max-height: 75vh; /* 전체 화면 사용 */
+  height: 80vh; /* 전체 화면 사용 */
 }
 
 /* 헤더 영역: 헤더는 고정되지 않고, 콘텐츠 영역과 별도로 분리 */
@@ -206,6 +206,7 @@ onUnmounted(() => {
 
 /* 콘텐츠 영역: 헤더 아래에서 스크롤 */
 .content {
+  height: 100%;
   overflow-y: auto;
 }
 
