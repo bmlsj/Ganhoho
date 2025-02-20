72ad80f17793c58ac50995b2cca4f3cdcbdc00d2
aoreo <hay6586@gmail.com>
2025-02-20 21:05:17 +0900

    style: 앱 이름 한글로 변경

--- ganhoho_app/app/src/main/java/com/ssafy/ganhoho/ui/AuthActivity.kt
+++ ganhoho_app/app/src/main/java/com/ssafy/ganhoho/ui/AuthActivity.kt
@@ -68,9 +68,9 @@ class AuthActivity : ComponentActivity() {
     fun navigateToMain(deepLinkUri: Uri?) {
         val intent = Intent(this, MainActivity::class.java).apply {
             deepLinkUri?.let { data = it }  // 딥링크 있으면 전달
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
         }
         startActivity(intent)
-        finish()
     }
 }
 
